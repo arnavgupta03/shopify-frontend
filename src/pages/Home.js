01109d8d4d326688af3bc7da45bc68391c774050
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2022-01-14 18:04:04 -0500

    Add title to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ class Home extends React.Component {
     }
 
     componentDidMount() {
-        fetch("https://api.nasa.gov/planetary/apod?count=10&api_key=s7uqRhZOYnUB9trgP02xD2pGJv0WMtde3Iu94flJ")
+        fetch("https://api.nasa.gov/planetary/apod?count=20&api_key=s7uqRhZOYnUB9trgP02xD2pGJv0WMtde3Iu94flJ")
             .then(response => response.json())
             .then(data => this.setState({page_loaded: true, image_array: data}));//this.setState({image_array: data}))
             // .then(() => console.log(this.state.image_array));
@@ -25,13 +25,21 @@ class Home extends React.Component {
 
     render() {
         return this.state.page_loaded 
-                    ? (<div className="row justify-content-center">
-                        <div className="col-sm-8 col-md-4">
-                            {
-                                this.state.image_array.map((item, index) => {
-                                    return <div key={index} className="row"><ImageCard key={index} link={item.url} imgTitle={item.title} date={item.date}/></div>
-                                })
-                            }
+                    ? (
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <h1 className="display-3 text-center">Assorted Images from NASA's APOD</h1>
+                            </div>
+                        </div>
+                        <div className="row justify-content-center">
+                            <div className="col-sm-8 col-md-4">
+                                {
+                                    this.state.image_array.map((item, index) => {
+                                        return <div key={index} className="row"><ImageCard key={index} link={item.url} imgTitle={item.title} date={item.date}/></div>
+                                    })
+                                }
+                            </div>
                         </div>
                     </div>)
                     : (
