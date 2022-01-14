00287768d5e74e9521e509dfcb8986459341055a
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2022-01-14 18:04:31 -0500

    Comment image card

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,16 +1,21 @@
+//import required libraries and components
 import React from "react";
 
+//component for image card
 class ImageCard extends React.Component
 {
     constructor(props) {
         super(props);
+        //set initial state
         this.state = {
             like_status: false,
         };
 
+        //bind like onclick function
         this.handleLike = this.handleLike.bind(this);
     }
 
+    //handle like by toggling state
     handleLike() {
         this.setState(prevState => ({
             like_status: !prevState.like_status
@@ -25,10 +30,13 @@ class ImageCard extends React.Component
                     <h3 className="card-title">{this.props.imgTitle}</h3>
                     <p className="card-text">{this.props.date}</p>
                     <button className="btn btn-primary" onClick={this.handleLike}>{this.state.like_status ? "Unlike" : "Like"}</button>
+                    {/* <label for="disabledLink">Get a link to the image here!</label>
+                    <input type="text" id="disabledLink" className="form-control" value={this.props.link}/> */}
                 </div>
             </div>
         );
     }
 }
 
+//export component for home page
 export default ImageCard;
