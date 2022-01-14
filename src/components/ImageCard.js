938d0d001483b7b1d6cb8eb8fb5b1d356aee43d4
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2022-01-14 16:47:06 -0500

    Change image card to class with like button

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,15 +1,34 @@
-function ImageCard(props)
+import React from "react";
+
+class ImageCard extends React.Component
 {
-    return (
-        <div className="card my-2">
-            <img src={props.link} className="card-img-top" alt={props.desc} />
-            <div className="card-body">
-                <h3 className="card-title">{props.imgTitle}</h3>
-                <p className="card-text">{props.date}</p>
-                <button className="btn btn-primary">Like</button>
+    constructor(props) {
+        super(props);
+        this.state = {
+            like_status: false,
+        };
+
+        this.handleLike = this.handleLike.bind(this);
+    }
+
+    handleLike() {
+        this.setState(prevState => ({
+            like_status: !prevState.like_status
+        }))
+    }
+
+    render() {
+        return (
+            <div className="card my-2">
+                <img src={this.props.link} className="card-img-top" alt={this.props.desc} />
+                <div className="card-body">
+                    <h3 className="card-title">{this.props.imgTitle}</h3>
+                    <p className="card-text">{this.props.date}</p>
+                    <button className="btn btn-primary" onClick={this.handleLike}>{this.state.like_status ? "Unlike" : "Like"}</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default ImageCard;
